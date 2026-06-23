e6bcfc6df4603f3d4ffe2ae4861201466f935133
SofyanBaharudinnn <sofyanbaharudin12@gmail.com>
2026-06-23 15:30:39 +0900

    feat: add WSGI configuration and serialize machine learning model file

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+# Konfigurasi GEMINI_API_KEY agar terbaca oleh settings.py
+os.environ['GEMINI_API_KEY'] = 'AIzaSyDHZYF3ILx5geJByBywfPN5TIob36_wOcs'
+
+# pyrefly: ignore [missing-import]
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
